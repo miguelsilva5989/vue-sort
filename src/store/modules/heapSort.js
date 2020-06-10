2975728f00d30374ae511feaedb1ed32891d138a
Mike <mike.5989@hotmail.com>
2020-06-10 18:43:48 +0200

    swap red color

--- src/store/modules/heapSort.js
+++ src/store/modules/heapSort.js
@@ -6,6 +6,11 @@ Vue.use(Vuex);
 export default {
   actions: {
     async createHeap({ dispatch, rootState }, { inputArr, length, index }) {
+      //if Stop button pressed
+      if (rootState.arrayManagement.forceStop) {
+        return;
+      }
+
       let max = index;
       let left = index * 2 + 1;
       let right = left + 1;
@@ -18,12 +23,35 @@ export default {
       }
 
       if (max != index) {
+        // speed can be changed during sorting
+        let sortSpeed = rootState.arrayManagement.selectedSortSpeed;
+        dispatch('arrayManagement/changeColor', {
+          index: index,
+          color: 'red',
+        });
+        dispatch('arrayManagement/changeColor', {
+          index: max,
+          color: 'red',
+        });
+        await dispatch('arrayManagement/timer', sortSpeed);
+
         var temp = inputArr[index];
         inputArr[index] = inputArr[max];
         rootState.arrayManagement.arrayToSort[index].y = inputArr[max];
 
         inputArr[max] = temp;
         rootState.arrayManagement.arrayToSort[max].y = temp;
+
+        dispatch('arrayManagement/changeColor', {
+          index: index,
+          color: 'blue',
+        });
+        dispatch('arrayManagement/changeColor', {
+          index: max,
+          color: 'blue',
+        });
+        await dispatch('arrayManagement/timer', sortSpeed);
+
         await dispatch('createHeap', {
           inputArr: inputArr,
           length: length,
@@ -51,12 +79,40 @@ export default {
       }
 
       while (j >= 0) {
+        //if Stop button pressed
+        if (rootState.arrayManagement.forceStop) {
+          return;
+        }
+        // speed can be changed during sorting
+        let sortSpeed = rootState.arrayManagement.selectedSortSpeed;
+
+        dispatch('arrayManagement/changeColor', {
+          index: 0,
+          color: 'red',
+        });
+        dispatch('arrayManagement/changeColor', {
+          index: j,
+          color: 'red',
+        });
+        await dispatch('arrayManagement/timer', sortSpeed);
+
         var temp = inputArr[0];
         inputArr[0] = inputArr[j];
         rootState.arrayManagement.arrayToSort[0].y = inputArr[j];
 
         inputArr[j] = temp;
         rootState.arrayManagement.arrayToSort[j].y = temp;
+
+        //change back to blue after swapping
+        dispatch('arrayManagement/changeColor', {
+          index: 0,
+          color: 'blue',
+        });
+        dispatch('arrayManagement/changeColor', {
+          index: j,
+          color: 'blue',
+        });
+
         await dispatch('createHeap', {
           inputArr: inputArr,
           length: j,
